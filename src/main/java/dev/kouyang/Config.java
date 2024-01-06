ad6ab51175cc838df4dbf4f5dde89b881227ca7e
Kouyang07 <154615729+Kouyang07@users.noreply.github.com>
2024-01-05 19:51:12 -0500

    Changed config

--- src/main/java/dev/kouyang/Config.java
+++ src/main/java/dev/kouyang/Config.java
@@ -1,6 +1,6 @@
 package dev.kouyang;
 
 public class Config {
-    public static final String API_KEY = "";
-    public static final String OpenAIEndPoint = "https://api.openai.com/v1/chat/completions";
+    public static String API_KEY = "";
+    public static String OpenAIEndPoint = "https://api.openai.com/v1/chat/completions";
 }
